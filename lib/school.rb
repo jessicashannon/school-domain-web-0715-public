7e43367b90d2367d20961454c6280bf86e5b480e
jessicashannon <jessdahling@gmail.com>
2015-08-05 13:50:03 -0400

    done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# Write code here
+require 'pry'
+
+class School
+
+  attr_reader :roster
+
+  def initialize(school_name)
+    @roster = {}
+    @name = school_name
+  end
+
+  def add_student(name, grade)
+    if @roster[grade] 
+      @roster[grade] << name
+    else 
+      @roster[grade] = [name]
+    end
+  end
+
+  def grade(number)
+    #@roster.select{|grade, student| grade == number}.values
+    @roster[number]
+  end
+
+  def sort
+    new_hash ={}
+    @roster.map {|key, value| new_hash[key] = value.sort}
+    new_hash
+  end
+
+end
